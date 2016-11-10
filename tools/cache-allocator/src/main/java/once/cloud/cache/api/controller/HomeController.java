fcb24ad41a2ef7ef6d8fd70981496df9ac6e9c1d
Zhen Tang <silenceofdaybreak@gmail.com>
2016-11-10 09:01:58 +0800

    cache-allocator: Stubs added.

---
+++ tools/cache-allocator/src/main/java/once/cloud/cache/api/controller/HomeController.java
@@ -0,0 +1,20 @@
+// ===============================================================
+// Copyright(c) Institute of Software, Chinese Academy of Sciences
+// ===============================================================
+// Author : Zhen Tang <tangzhen12@otcaix.iscas.ac.cn>
+// Date   : 2016/11/10
+
+package once.cloud.cache.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	@ResponseBody
+	public String index() {
+		return "OnceCloud Cache Allocator";
+	}
+}
