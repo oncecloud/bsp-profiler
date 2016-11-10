e87883c8b4bd8b3f4118ab7ea26f9975a5425f17
Zhen Tang <silenceofdaybreak@gmail.com>
2016-11-10 09:11:19 +0800

    cache-allocator: ProcessHelper added.

---
+++ tools/cache-allocator/src/main/java/once/cloud/cache/helper/ProcessHelper.java
@@ -0,0 +1,65 @@
+// ===============================================================
+// Copyright(c) Institute of Software, Chinese Academy of Sciences
+// ===============================================================
+// Author : Zhen Tang <tangzhen12@otcaix.iscas.ac.cn>
+// Date   : 2016/11/10
+
+package once.cloud.cache.helper;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProcessHelper {
+	public CommandResult run(String commandLine) {
+		try {
+			final Process process = Runtime.getRuntime().exec(new String[] { "/bin/sh", "-c", commandLine });
+			final StringBuilder standardOutput = new StringBuilder();
+			final StringBuilder standardError = new StringBuilder();
+			Thread readStandardOutput = new Thread() {
+				public void run() {
+					try {
+						BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+						int c = -1;
+						try {
+							while ((c = reader.read()) != -1) {
+								standardError.append((char) c);
+							}
+						} finally {
+							reader.close();
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			};
+			Thread readStandardError = new Thread() {
+				public void run() {
+					try {
+						BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+						int c = -1;
+						try {
+							while ((c = reader.read()) != -1) {
+								standardError.append((char) c);
+							}
+						} finally {
+							reader.close();
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			};
+			readStandardOutput.start();
+			readStandardError.start();
+			int exitValue = process.waitFor();
+			readStandardOutput.join();
+			readStandardError.join();
+			return new CommandResult(exitValue, standardOutput.toString(), standardError.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
